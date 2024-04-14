c5026442ec744e222c903f1d94b5b69d2eac52f7
Sinskiy <sinskiy@proton.me>
2024-04-14 13:58:50 +0300

    buttons styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <div>
+        <input type="time" name="time" id="time" />
+        <input type="text" name="title" id="title" />
+        <button className="filled">Add</button>
+      </div>
+      <div data-active>
+        <p>
+          <time dateTime="">17:00</time>-<time dateTime="21:00">21:00</time>
+        </p>
+        <p>Domra</p>
+        <button className="outlined">Edit</button>
+        <button className="outlined danger">Delete</button>
+      </div>
+    </main>
+  );
 }
