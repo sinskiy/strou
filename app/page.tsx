876c7a9b6fe92e6bae6ec9b4f0b45f9a845f1b4d
Sinskiy <sinskiy@proton.me>
2024-04-14 16:18:40 +0300

    add disabled button styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 export default function Home() {
   return (
     <main>
-      <button>hello</button>
       <button className="filled">hello</button>
       <button className="outlined">hello</button>
       <button className="tonal">hello</button>
       <button className="text">hello</button>
+      <button disabled className="filled">
+        hello
+      </button>
+      <button disabled className="outlined">
+        hello
+      </button>
+      <button disabled className="tonal">
+        hello
+      </button>
+      <button disabled className="text">
+        hello
+      </button>
     </main>
   );
 }
