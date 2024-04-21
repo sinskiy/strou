18fbb1e79fc8d621ce203c7ea453fc402a70c609
Sinskiy <sinskiy@proton.me>
2024-04-21 12:31:42 +0300

    make buttons fully rounded

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -14,7 +14,7 @@ const Button = ({
   ...attributes
 }: ButtonProps) => {
   const before =
-    'relative before:content-[""] before:absolute before:-z-10 before:rounded-md before:opacity-0 before:scale-0 before:left-0 before:top-0 before:right-0 before:bottom-0 before:duration-300 before:hover:scale-100 before:hover:opacity-80 active:opacity-100';
+    'relative before:content-[""] before:absolute before:-z-10 before:rounded-full before:opacity-0 before:scale-0 before:left-0 before:top-0 before:right-0 before:bottom-0 before:duration-300 before:hover:scale-100 before:hover:opacity-80 active:opacity-100';
   const variants = {
     text: `text-on-${colors} before:bg-${colors} ${before}`,
     filled: `interactive-bg-${colors}`,
@@ -23,7 +23,7 @@ const Button = ({
   return (
     <button
       className={twMerge(
-        "inline-flex items-center jusify-center gap-2 px-6 py-4 rounded-md font-medium text-base transition-[transform,background-color,color] active:scale-95 disabled:pointer-events-none disabled:opacity-50",
+        "inline-flex items-center jusify-center gap-2 px-8 py-4 rounded-full font-medium text-base transition-[transform,background-color,color] active:scale-95 disabled:pointer-events-none disabled:opacity-50",
         variants[variant],
         className
       )}
