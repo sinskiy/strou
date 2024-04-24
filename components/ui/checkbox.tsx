5a9e1bb2020544fa0e9d83c9d30d02670454e830
Sinskiy <sinskiy@proton.me>
2024-04-24 20:12:22 +0300

    improved checkbox checked styling and check animation

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -1,5 +1,6 @@
 import { InputHTMLAttributes } from "react";
 import { twMerge } from "tailwind-merge";
+import CheckIcon from "../icons/checkIcon";
 
 export interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
   checked: boolean;
@@ -11,12 +12,14 @@ const Checkbox = ({ checked, className, ...props }: CheckboxProps) => {
       role="checkbox"
       aria-checked={checked}
       className={twMerge(
-        "size-6 bg-transparent border-2 border-outline rounded-md flex justify-center items-center focus-visible:outline-primary transition-[transform,background-color,color] active:scale-95 aria-checked:bg-primary aria-checked:border-primary disabled:pointer-events-none disabled:opacity-50",
+        "size-6 bg-transparent border-2 border-outline rounded-md focus-visible:outline-primary transition-[transform,background-color,color] duration-300 active:scale-95 aria-checked:bg-primary aria-checked:border-primary disabled:pointer-events-none disabled:opacity-50",
         className,
       )}
       {...props}
     >
-      {checked && "✔"}
+      <CheckIcon
+        className={`${checked ? "opacity-100 delay-200" : "opacity-0"} transition-opacity`}
+      />
     </div>
   );
 };
