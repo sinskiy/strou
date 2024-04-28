164588a9ce0f161d6c374595ddc61af7b41f2645
Sinskiy <sinskiy@proton.me>
2024-04-28 11:51:12 +0300

    add static shortcut helper

--- components/newTaskForm.tsx
+++ components/newTaskForm.tsx
@@ -16,15 +16,18 @@ export default function NewTaskForm({
 }: NewTaskFormProps) {
   return (
     <form onSubmit={handleAddTodo} className="flex flex-wrap gap-4">
-      <input
-        value={newTask}
-        onChange={(e) => setNewTask(e.target.value)}
-        type="text"
-        placeholder="what do you want to do?"
-        name="task-title"
-        id="task-title"
-        className="w-[calc(100%-5rem)] sm:w-auto"
-      />
+      <div className="relative w-[calc(100%-5rem)] sm:w-auto">
+        <input
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          type="text"
+          placeholder="what do you want to do?"
+          name="task-title"
+          id="task-title"
+          className="w-full"
+        />
+        <kbd className="absolute right-6 top-4">ctrl+k</kbd>
+      </div>
       <Button disabled={!newTask} className="p-4 sm:px-8">
         <Add />
         <span className="hidden sm:inline-block">add</span>
