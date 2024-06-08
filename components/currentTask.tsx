8d35e3d623205d8f862d07a59499917ae1396058
sinskiy <sinskiy@proton.me>
2024-06-08 08:32:04 +0300

    get current task in timer

--- components/currentTask.tsx
+++ components/currentTask.tsx
@@ -2,20 +2,39 @@ import { Checkbox } from "./ui/checkbox";
 import { Label } from "./ui/label";
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { useState, useEffect } from "react";
+import { Task } from "./tasks";
 
 interface Props {}
 export default function CurrentTask({}: Props) {
+  const [savedCurrentTask, setSavedCurrentTask] = useState<null | Task>(null);
+  useEffect(() => {
+    const savedCurrentTaskIndex = Number(localStorage.currentTask);
+    const tasks = JSON.parse(localStorage.tasks ?? "[]");
+    if (savedCurrentTaskIndex >= 0) {
+      const currentTask = tasks.find(
+        (task: Task) => task.originalIndex === savedCurrentTaskIndex,
+      );
+      setSavedCurrentTask(currentTask);
+    }
+  }, []);
   return (
-    <section className="card flex items-center gap-4">
-      <Label className="flex gap-3">
-        <Checkbox />
-        <p>
-          play the domra <span className="opacity-30">is current task</span>
-        </p>
-      </Label>
-      <Button variant="secondary" size="sm" asChild>
-        <Link href="/tasks">change</Link>
-      </Button>
-    </section>
+    <>
+      {savedCurrentTask && (
+        <section className="card flex items-center gap-4">
+          <Label className="flex gap-3">
+            {/* TODO: add ability to check  */}
+            <Checkbox checked={savedCurrentTask.checked} />
+            <p>
+              <span>{savedCurrentTask.title} </span>
+              <span className="opacity-30">is current task</span>
+            </p>
+          </Label>
+          <Button variant="secondary" size="sm" asChild>
+            <Link href="/tasks">change</Link>
+          </Button>
+        </section>
+      )}
+    </>
   );
 }
