cdc1054327ccd40c8c237fff4f334a2699ee8194
sinskiy <sinskiy@proton.me>
2024-06-09 07:56:39 +0300

    don't show hours if time is less than 60 minutes

--- components/timestamp.tsx
+++ components/timestamp.tsx
@@ -8,9 +8,13 @@ interface TimestampProps {
 export default function Timestamp({ timeObject }: TimestampProps) {
   return (
     <p className="font-bold text-6xl mt-2 mb-6">
-      {Object.entries(timeObject).map(([label, value], i) => (
-        <FormattedTimeUnit key={label} timeUnit={value} separator={i !== 2} />
-      ))}
+      {Object.entries(timeObject).map(([label, value], i) => {
+        return label === "hours" && value < 1 ? (
+          ""
+        ) : (
+          <FormattedTimeUnit key={label} timeUnit={value} separator={i !== 2} />
+        );
+      })}
     </p>
   );
 }
