430bbf15a8b82b365a2e64697fafea0ad4f3d407
sinskiy <sinskiy@proton.me>
2024-06-10 10:05:19 +0300

    chore: remove unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { twMerge } from "tailwind-merge";
-import { PropsWithChildren, ReactNode } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
