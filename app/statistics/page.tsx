5d42be1e74094d4428bac7ff351add0b8e95cab1
sinskiy <sinskiy@proton.me>
2024-06-13 14:43:20 +0300

    feat: awfullly parse statistics

--- app/statistics/page.tsx
+++ app/statistics/page.tsx
@@ -1,3 +1,30 @@
+"use client";
+
+import { Statistic } from "@/lib/statistics";
+import { useEffect, useState } from "react";
+
 export default function Statistics() {
-  return <h1>hello world</h1>;
+  const [statistics, setStatistics] = useState<null | Statistic[]>(null);
+  const statisticTitle = (statistic: Statistic) => {
+    if (statistic.checked) {
+      return `${statistic.title} was ${statistic.checked ? "checked" : "unchecked"}`;
+    } else if (statistic.originalTitle) {
+      return `${statistic.originalTitle}'s title was changed ${statistic.title}`;
+    }
+  };
+  useEffect(() => {
+    const savedStatistics: Statistic[] = JSON.parse(
+      localStorage.statistics ?? "[]",
+    );
+    setStatistics(savedStatistics);
+  }, []);
+  return (
+    <section className="card">
+      {statistics
+        ? statistics.map((statistic) => (
+            <p key={statistic.id}>{statisticTitle(statistic)}</p>
+          ))
+        : "there is no statistics"}
+    </section>
+  );
 }
